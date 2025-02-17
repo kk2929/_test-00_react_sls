6dcb1cbf5d6d3809c38633e85fbc85d8d60d104d
kume <kkc.birth@gmail.com>
2025-02-17 23:42:41 +0900

    add: jest

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,3 +1,43 @@
+// /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-	
-}
+  roots: ["<rootDir>"],
+  testMatch: ["**/?(*.)+(spec).+(ts)"],
+  transform: {
+    "^.+\\.(ts)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  // collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!.serverless/**",
+  ],
+  coverageDirectory: "coverage_dir",
+  coverageReporters: ["html"],
+  moduleNameMapper: {
+    "^@/(.+)$": "<rootDir>/src/$1",
+  },
+  // reporters: [
+  //   "default",
+  //   [
+  //     "jest-html-reporters",
+  //     {
+  //       pageTitle: "Test Report",
+  //       publicPath: "./tests/report/jest",
+  //       filename: `api-unit-test-report.html`,
+  //       expand: true,
+  //       inlineSource: true,
+  //     },
+  //   ],
+  // ],
+};
+// preset: "ts-jest",
+// testEnvironment: "node",
+// roots: ["<rootDir>/tests", "<rootDir>/src"],
+// collectCoverageFrom: ["**/*.ts", "!**/node_modules/**"],
+// moduleDirectories: ["node_modules", ".", "<rootDir>"],
--- backend/tests/test.spec.ts
+++ backend/tests/test.spec.ts
@@ -1,3 +1,25 @@
+import "reflect-metadata"; // required by inversify
+import { Container, injectable } from "inversify";
+import { TYPES } from "@/config/dependency/types";
+import type { IHelloDepend } from "@/types/interface.d";
 import { Hello } from "@/functions/hello/usecase";
 
-describe("first", () => {});
+const container = new Container({ defaultScope: "Singleton" });
+
+// usecases
+container.bind<Hello>(TYPES.Hello).to(Hello);
+
+@injectable()
+class HelloDepend implements IHelloDepend {
+  say(word: string): string {
+    return `${word} desuyo`;
+  }
+}
+container.bind<IHelloDepend>(TYPES.HelloDepend).to(HelloDepend);
+
+describe("first", () => {
+  test("should first", () => {
+    const hello = container.get<Hello>(TYPES.Hello);
+    expect(hello instanceof Hello).toBe(true);
+  });
+});
